500f3e78191cd31ea90379402e856d7f188197dd
mithileshssingh <mithilesh.singh@blackbuck.com>
2024-03-09 22:53:23 +0530

    Initial Commit

---
+++ BusinessCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BusinessCard
+//
+//  Created by Mithilesh Singh on 09/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
